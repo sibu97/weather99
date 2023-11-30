9b8a619072dad3f7565bf2d2c66a7bdf4281e109
SIBANANDA NAYAK <57071664+sibu97@users.noreply.github.com>
2023-11-30 18:29:53 +0000

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import WeatherCard from './components/WeatherCard.jsx';
+// import React, { useState, useEffect } from 'react';
+// import WeatherCard from './components/WeatherCard.jsx';
 import Navbar from './components/Navbar.jsx'
 
-import logo from './weather-app.png';
+// import logo from './weather-app.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import '../assets/css/navbar.css'
-import Logo from '../weather-app.png'
-const Navbar = () => {
+import Icon from '../assets/images/icons8-rain-cloud.gif'
+import { WiRefresh } from "react-icons/wi";
+
+const Navbar = ({setStartDate,setState,setCity,setLocation}) => {
+
+
   return (
-    <div className='brand' style={{marginTop:0,
-        height:'60px',
-        width:'100%',
-        backgroundColor: '#74EBD5',
-        backgroundImage: 'linear-gradient(90deg, #74ABD5 0%, #9F9CE6 100%)',
-          boxShadow: ' 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-          display:'flex',
-      }}>
-          <img alt='brndlogo' csrc={Logo} className="App-logo"  style={{padding:'0px',height:'5%',width:'5%'}}/>
-          <h1 style={{color:'ButtonShadow',fontFamily:'sans-serif',fontSize:'30px'}}>Weather Report</h1>
+    <div className='brand'>
+         <div className="flex justify-between bg-[#000000] bg-opacity-50 w-screen p-2 items-center refr">      
+                    <img alt='brndlogo' c src={Icon} className="App-log" style={{backgroundColor:'black',}} />
+                    <h1 style={{color:'ButtonShadow',fontFamily:'sans-serif',fontSize:'30px'}}>Weather Report</h1>
+          </div>
+
+          <div onClick={()=>{setStartDate("");setState("");setCity("");setLocation("")}} className="refr1">
+                  <WiRefresh style={{height:'50px',width:'50px',color:'white'}} /> 
+                  <h1>Refresh </h1>    
+          </div>
+
       </div>
   )
 }
